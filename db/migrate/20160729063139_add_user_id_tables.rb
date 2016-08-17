404bcafcfd6252d1bece02c862156829c54fba67
VIBHOR94 <vibhorgupta2008@aol.in>
2016-08-17 12:15:55 +0530

    initial commit5

--- db/migrate/20160729063139_add_user_id_tables.rb
+++ db/migrate/20160729063139_add_user_id_tables.rb
@@ -1,6 +1,6 @@
 class AddUserIdTables < ActiveRecord::Migration
   def change
-  	add_column :clients ,:user_id ,:inetger
+  	add_column :clients ,:user_id ,:integer
   	add_column :projects ,:user_id,:integer
   end
 end
